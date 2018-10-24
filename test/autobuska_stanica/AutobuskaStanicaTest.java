69f3685c68f858ee1188d7d5cfad74f90ede0033
bojantomic <tomicb@fon.rs>
2018-10-24 10:22:19 +0200

    Gotovi testovi za Polazak i StringIspisivac

---
+++ test/autobuska_stanica/AutobuskaStanicaTest.java
@@ -0,0 +1,34 @@
+package autobuska_stanica;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AutobuskaStanicaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void metoda_UnesiPolazak() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void metoda_RezervisiKarte() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void metoda_ProslediRezervaciju() {
+		fail("Not yet implemented");
+	}
+
+}
